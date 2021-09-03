c5ef005a54b996ec8fb2cff182747271998342e1
Nick Tiart <n.tiart@abonentplus.ru>
2021-09-03 09:16:58 +0300

    BroadcastWidgetExample refactoring

--- BroadcastWidgetExample/app/src/main/java/com/rznnike/broadcastwidgetexample/BroadcastWidget.kt
+++ BroadcastWidgetExample/app/src/main/java/com/rznnike/broadcastwidgetexample/BroadcastWidget.kt
@@ -1,11 +1,13 @@
 package com.rznnike.broadcastwidgetexample
 
+import android.annotation.SuppressLint
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.widget.RemoteViews
 
 /**
@@ -13,9 +15,9 @@ import android.widget.RemoteViews
  */
 class BroadcastWidget : AppWidgetProvider() {
     override fun onUpdate(
-            context: Context,
-            appWidgetManager: AppWidgetManager,
-            appWidgetIds: IntArray
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
     ) { // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
@@ -42,17 +44,31 @@ class BroadcastWidget : AppWidgetProvider() {
         private var mCounter = 0
 
         fun updateAppWidget(
-                context: Context,
-                appWidgetManager: AppWidgetManager,
-                appWidgetId: Int
+            context: Context,
+            appWidgetManager: AppWidgetManager,
+            appWidgetId: Int
         ) { // Construct the RemoteViews object
             val views = RemoteViews(context.packageName, R.layout.broadcast_widget)
             // Construct an Intent which is pointing this class.
             val intent = Intent(context, BroadcastWidget::class.java)
             intent.action = ACTION_SIMPLE_APP_WIDGET
-            // And this time we are sending a broadcast with getBroadcast
-            val pendingIntent = PendingIntent.getBroadcast(context, 0, intent,
-                    PendingIntent.FLAG_UPDATE_CURRENT)
+            // And this time we are sending a broadcast with
+            @SuppressLint("UnspecifiedImmutableFlag")
+            val pendingIntent = if (Build.VERSION.SDK_INT >= 23) {
+                PendingIntent.getBroadcast(
+                    context,
+                    0,
+                    intent,
+                    PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+                )
+            } else {
+                PendingIntent.getBroadcast(
+                    context,
+                    0,
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+            }
             views.setOnClickPendingIntent(R.id.tvWidget, pendingIntent)
             // Instruct the widget manager to update the widget
             appWidgetManager.updateAppWidget(appWidgetId, views)
